ff8f7138b3a7fce615ebebf289af5d7899f75257
FezLight <alex_se@hotmail.fr>
2025-08-10 02:24:07 +0200

    fix: logging condition

--- mine4ease-backend/src/config/ObjectFactoryConfig.ts
+++ mine4ease-backend/src/config/ObjectFactoryConfig.ts
@@ -14,7 +14,7 @@ process.env.APP_DIRECTORY = path.join(app.getPath('appData'), '.mine4ease');
 process.env.LOG_DIRECTORY = process.env.APP_DIRECTORY + '/logs'
 
 export const logger = createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  level: app.isPackaged ? 'info' : 'debug',
   format: combine(
     timestamp(),
     format.json()
@@ -33,7 +33,7 @@ export const logger = createLogger({
   handleRejections: true
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (!app.isPackaged) {
   logger.add(new winston.transports.Console());
 }
 
