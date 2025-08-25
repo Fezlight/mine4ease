023c448520ca996755cb60c6ef81d2c20a84a809
FezLight <alex_se@hotmail.fr>
2025-08-25 04:40:32 +0200

    fix: incorrect conf

--- mine4ease-backend/src/config/ObjectFactoryConfig.ts
+++ mine4ease-backend/src/config/ObjectFactoryConfig.ts
@@ -14,7 +14,7 @@ process.env.APP_DIRECTORY = path.join(app.getPath('appData'), '.mine4ease');
 process.env.LOG_DIRECTORY = process.env.APP_DIRECTORY + '/logs'
 
 export const logger = createLogger({
-  level: app.isPackaged ? 'debug' : 'debug',
+  level: app.isPackaged ? 'info' : 'debug',
   format: combine(
     timestamp(),
     format.json()
